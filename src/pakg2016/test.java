4f75ea59c78a908ea6e63dfa4745bc95834fd057
HARIS <HARIS@192.168.10.39>
2016-06-20 03:14:15 +0500

    TIPU G

---
+++ src/pakg2016/test.java
@@ -0,0 +1,16 @@
+package pakg2016;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void test() {
+		add a=new add();
+		int result=a.ad(2,4);
+		assertEquals(result,5);
+	}
+
+}
